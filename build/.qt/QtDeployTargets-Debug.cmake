1bdbc5ac6af9e65e8fd26291729b431039fe9b1d
骆唐文 <1943055307@qq.com>
2025-11-04 17:22:09 +0800

    Remove build dir from repo & add .gitignore

--- build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_bool_FILE E:/Doctor/class/fir/fall/cad/final/bool/build/Debug/bool.exe)
-set(__QT_DEPLOY_TARGET_bool_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_bool_RUNTIME_DLLS C:/QT6/6.9.3/msvc2022_64/bin/Qt6OpenGLWidgetsd.dll;C:/QT6/6.9.3/msvc2022_64/bin/Qt6Widgetsd.dll;C:/QT6/6.9.3/msvc2022_64/bin/Qt6OpenGLd.dll;C:/QT6/6.9.3/msvc2022_64/bin/Qt6Guid.dll;C:/QT6/6.9.3/msvc2022_64/bin/Qt6Cored.dll)
--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_bool_FILE E:/Doctor/class/fir/fall/cad/final/bool/build/MinSizeRel/bool.exe)
-set(__QT_DEPLOY_TARGET_bool_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_bool_RUNTIME_DLLS C:/QT6/6.9.3/msvc2022_64/bin/Qt6OpenGLWidgets.dll;C:/QT6/6.9.3/msvc2022_64/bin/Qt6Widgets.dll;C:/QT6/6.9.3/msvc2022_64/bin/Qt6OpenGL.dll;C:/QT6/6.9.3/msvc2022_64/bin/Qt6Gui.dll;C:/QT6/6.9.3/msvc2022_64/bin/Qt6Core.dll)
